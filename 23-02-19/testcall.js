aa6ecde4fd00684092416731cd85133131b0a4e2
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-23 21:22:27 -0500

    Testing more specific requests

--- 23-02-19/testcall.js
+++ 23-02-19/testcall.js
@@ -14,15 +14,32 @@ var dis = new discogs({
     console.log(results);
 })*/
 
-db.getRelease(13933)
+/* db.getRelease(13933)
     .then((release)=>{
         return db.getArtist(release.artists[0].id);
     })
     .then((artist)=>{
         console.log(artist.name);
-    })
+    }) */
 
-db.getArtist(28085,(err,results)=>{
+/* db.getArtist(28085,(err,results)=>{
     if(err){throw err}
     console.log(results);
-})
+}) */
+
+/* db.getArtistReleases(28085,(err,results)=>{
+    if(err){throw err}
+    for(let i=0;i<results.releases.length;i++){
+        console.log(results.releases[i].title);
+    }
+}) */
+
+db.getArtistReleases(133866,(err,results)=>{
+    if(err){throw err}
+    for(let i=0;i<results.releases.length;i++){
+        if(results.releases[i].year < 2000 && results.releases[i].artist === 'UGK'){
+            console.log(results.releases[i]);
+            //console.log(results.releases[i].title + "- " + results.releases[i].year);
+        }
+    }
+});
